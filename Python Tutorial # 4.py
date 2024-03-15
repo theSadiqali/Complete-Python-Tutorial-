66ae1d954dce5e10221a29b340cf5bf051bf8b80
Sadiq Ali <142330669+theSadiqali@users.noreply.github.com>
2024-03-15 23:43:12 +0500

    Add files via upload

---
+++ Python Tutorial # 4.py
@@ -0,0 +1,30 @@
+'''
+Course Contents
+'''
+# 1 Intro to Python ---> Its done 
+# 2 String in Python 
+# 3 Lists in Python 
+# 4 Tuples in Python 
+# 5 Sets in Python 
+# 6 Dictionaries in Python
+# 7 Conditions in Python 
+# 8 Loops in Python 
+# 9 Function in Python 
+# 10 Exception Handling in Python 
+# 11 Built in Functions in Python 
+# 12 Classes and Objects in Python 
+# 13 Reading Files in Python 
+# 14 Writing Files in Python 
+# 15 String operator and Functions in Python 
+# 16 Array in Python 
+# 17 Lambda in Python 
+# 18 Math Module Function in Python 
+# 19 List COmprehension in Python 
+# 20 Decorators in Python Generator in Python
+# 21 Five simple and Easy Python Projects with Source Code 
+# 22 Numpy 
+# 23 Pandas 
+# 24 Tensor Flow / Pytorch 
+# 25 Another Five Project with Source Code
+# 26 AI Basics 
+# 27 ML Basics 
